ee5d0375f2b7236a01e289f00bc463e736cc495c
Wilk Maia <wilkmaia@gmail.com>
2023-01-12 13:14:43 -0300

    feat: add cities controller

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,33 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API;
+
+public class CitiesDataStore
+{
+    public List<CityDto> Cities { get; set; }
+    public static CitiesDataStore Current { get; } = new CitiesDataStore();
+    
+    public CitiesDataStore()
+    {
+        Cities = new List<CityDto>()
+        {
+            new CityDto()
+            {
+                Id = 1,
+                Name = "Teresina",
+                Description = "Capital do Piauí",
+            },
+            new CityDto()
+            {
+                Id = 2,
+                Name = "Timon",
+                Description = "Quintal de Teresina",
+            },
+            new CityDto()
+            {
+                Id = 3,
+                Name = "São João dos Patos",
+            },
+        };
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerator<CityDto>> GetCities()
+    {
+        return Ok(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<CityDto> GetCity(int id)
+    {
+        CityDto? city = CitiesDataStore.Current.Cities.Find(city => city.Id == id);
+        if (city == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(city);
+    }
+}
