e31e6c8a8d1e09be424077cfc9ca077ee647a5e0
Wilk Maia <wilkmaia@gmail.com>
2023-01-16 14:39:20 -0300

    feat: add authentication route
    Authentication route validates username and password match (TODO) and return a JSON Web Token (JWT) that will be used to identify the user in other requests.

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,82 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+
+    private class CityInfoUser
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+        {
+            UserId = userId;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+    }
+
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationController(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+    
+    [HttpPost("authenticate")]
+    public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+    {
+        var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var securityKey =
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"] ?? string.Empty));
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claimsForToken = new()
+        {
+            new Claim("sub", user.UserId.ToString()),
+            new Claim("given_name", user.FirstName),
+            new Claim("family_name", user.LastName),
+            new Claim("city", user.City),
+        };
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            _configuration["Authentication:Issuer"],
+            _configuration["Authentication:Audience"],
+            claimsForToken,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddHours(1),
+            signingCredentials
+        );
+
+        return Ok(new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken));
+    }
+
+    private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+    {
+        // TODO - move user data to database
+        return new CityInfoUser(1, "sampleuser", "Sample", "User", "Teresina");
+    }
+}
